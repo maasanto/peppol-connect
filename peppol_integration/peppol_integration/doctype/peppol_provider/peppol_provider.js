0a79414acd58a94555ebaff4072af99a5bc46d8f
Antoine Maas <antoine.maas@gmail.com>
2025-10-21 13:53:18 +0200

    feat: new doctype peppol provider

---
+++ peppol_integration/peppol_integration/doctype/peppol_provider/peppol_provider.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Antoine Maas and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Peppol Provider", {
+// 	refresh(frm) {
+
+// 	},
+// });
