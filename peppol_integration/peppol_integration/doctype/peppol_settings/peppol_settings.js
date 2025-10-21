13b9b04c90212ad7b9935be146a080473a85e312
Antoine Maas <antoine.maas@gmail.com>
2025-10-21 13:53:28 +0200

    feat: new doctype peppol settings

---
+++ peppol_integration/peppol_integration/doctype/peppol_settings/peppol_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Antoine Maas and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Peppol Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
